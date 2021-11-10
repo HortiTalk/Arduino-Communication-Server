835bb3de21326e63d8e01d96bdb920d0f4564999
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2021-11-09 20:16:38 -0400

    Atualizado codigo

--- RegisterDataServerApplication/RegisterDataServerApplication.ino
+++ RegisterDataServerApplication/RegisterDataServerApplication.ino
@@ -5,17 +5,20 @@
 #include <ESP8266HTTPClient.h>
 
 // este parâmetro altera conforme a validade do certificado
-const char* fingerprint = "C3:75:80:CA:88:E2:40:62:94:8F:E9:E9:BD:18:81:B8:33:39:AA:33";
+const char* fingerprint = "E1:87:83:58:FE:6B:0E:DF:61:F2:12:AE:D2:F2:C3:7F:A2:70:6C:5C";
 
 const char* ssid = "NOME DA REDE";
 const char* password = "SENHA DA REDE";
-const char* host = "https://hortitalk-api.herokuapp.com"; // URL para o servidor
+const char* host = "https://hortitalk-server.herokuapp.com"; // URL para o servidor
 const String boardId = WiFi.macAddress();
 
 // Propriedades para passar para registro e busca no servidor
 const String TEMPERATURA = "temperatura";
 const String UMIDADE = "umidade";
-const String BOARD_ID = "board_id";
+const String UMIDADE_SOLO = "umidade";
+const String CULTIVO = "cultivo";
+const String BOARD_ID = "codigo";
+const String MODULO = "modulo";
 
 boolean placaExiste = false;
 
@@ -25,6 +28,7 @@ HTTPClient https;
 
 int temperatura = 20;
 int umidade = 21;
+int umidade_solo = 30;
 
 void setup() {
   Serial.begin(115200);
@@ -47,14 +51,17 @@ void loop() {
 
     // Registrar Placa, caso não esteja cadastrada
     if (!placaExiste) {
-      registrarInformacoesServidor(construirUrlRegistrarPlaca(), BOARD_ID, boardId);
+      registrarInformacoesServidorModulo(construirUrlRegistrarPlaca(), BOARD_ID, boardId, CULTIVO, 1);
     }
     
     // Registrar Temperatura
-    registrarInformacoesServidor(construirUrlRegistrarTemperatura(), TEMPERATURA, temperatura++);
+    registrarInformacoesServidorModulo(construirUrlRegistrarTemperatura(), TEMPERATURA, temperatura++, MODULO, boardId);
 
     // Registrar Umidade
-    registrarInformacoesServidor(construirUrlRegistrarUmidade(), UMIDADE, umidade++);
+    registrarInformacoesServidorModulo(construirUrlRegistrarUmidade(), UMIDADE, umidade++, MODULO, boardId);
+
+    // Registrar Umidade Solo
+    registrarInformacoesServidorModulo(construirUrlRegistrarUmidadeSolo(), UMIDADE_SOLO, umidade++, MODULO, boardId);
   }
   delay(5000);
 }
@@ -76,10 +83,11 @@ void verificarSePlacaJaRegistrada() {
   }
 }
 
-void registrarInformacoesServidor(String url, String propriedade, int valor) {
+void registrarInformacoesServidorModulo(String url, String propriedade, String valor, String propriedadeRelacionamento, int valorRelacionamento) {
   if(https.begin(*client, url)) {
     DynamicJsonDocument documentJson(1024);
     documentJson[propriedade] = valor;
+    documentJson[propriedadeRelacionamento] = valorRelacionamento;
         
     String json;
     serializeJson(documentJson, json);
@@ -98,10 +106,11 @@ void registrarInformacoesServidor(String url, String propriedade, int valor) {
   }
 }
 
-void registrarInformacoesServidor(String url, String propriedade, String valor) {
+void registrarInformacoesServidorModulo(String url, String propriedade, int valor, String propriedadeRelacionamento, String valorRelacionamento) {
   if(https.begin(*client, url)) {
     DynamicJsonDocument documentJson(1024);
     documentJson[propriedade] = valor;
+    documentJson[propriedadeRelacionamento] = valorRelacionamento;
         
     String json;
     serializeJson(documentJson, json);
@@ -121,17 +130,21 @@ void registrarInformacoesServidor(String url, String propriedade, String valor) 
 }
 
 String construirUrlVerificarPlacaExiste() {
-  return String(host) + "/board/" + boardId + "/exists";
+  return String(host) + "/modulos/" + boardId + "/existe";
 }
 
 String construirUrlRegistrarPlaca() {
-  return String(host) + "/board/";
+  return String(host) + "/modulos/";
 }
 
 String construirUrlRegistrarTemperatura() {
-  return String(host) + "/temperatura/board/" + boardId;
+  return String(host) + "/temperaturas/";
 }
 
 String construirUrlRegistrarUmidade() {
-  return String(host) + "/umidade/board/" + boardId;
+  return String(host) + "/umidades/";
+}
+
+String construirUrlRegistrarUmidadeSolo() {
+  return String(host) + "/umidades-solo/";
 }
